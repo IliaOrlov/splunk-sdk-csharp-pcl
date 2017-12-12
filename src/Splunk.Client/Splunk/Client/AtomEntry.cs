5b78a8849614b4656b0c6f4f40378b374d3c1fca
IliaOrlov <iliya.orlov@outlook.com>
2017-12-12 13:14:15 +0300

    returned headers with license (were removed by contracts removal tool)

--- src/Splunk.Client/Splunk/Client/AtomEntry.cs
+++ src/Splunk.Client/Splunk/Client/AtomEntry.cs
@@ -1,3 +1,26 @@
+/*
+ * Copyright 2014 Splunk, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"): you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+//// TODO:
+//// [ ] Performance: NameTable could make in AtomEntry.ReadXmlAsync and 
+////     AtomFeed.ReadXmlAsync significantly faster.
+//// [ ] Synchronization: AtomFeed.ReadXmlAsync and AtomEntry.ReadXmlAsync can
+////     be called more than once. (In practice these methods are never called
+////     move than once.)
+
 ﻿namespace Splunk.Client
 {
     using System;
